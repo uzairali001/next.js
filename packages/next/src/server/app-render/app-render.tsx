0f3a2ae2dbac8e12425075c7572dfc9f1068ab1e
Jiachi Liu <inbox@huozhi.im>
2023-08-13 22:14:53 +0200

    revert

--- packages/next/src/server/app-render/app-render.tsx
+++ packages/next/src/server/app-render/app-render.tsx
@@ -650,7 +650,6 @@ export async function renderToHTMLOrFlight(
 
       const [NotFound, notFoundStyles] = notFound
         ? await createComponentAndStyles({
-            shouldPreload: true,
             filePath: notFound[1],
             getComponent: notFound[0],
             injectedCSS: injectedCSSWithCurrentLayout,
